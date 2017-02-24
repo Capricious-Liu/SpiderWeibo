ce4fe8d7376f2ffb04835cf6f60e74335afd733c
Capricious-Liu <1073715498@qq.com>
2017-02-24 15:58:45 +0800

    Name TwitterContext & File Operation

--- readme.md
+++ readme.md
@@ -31,3 +31,19 @@ Be able to search microblog depending on keyword.</br>
 Unfortunately, it is found that Weibo.cn dont provide LOCATION information *(actually it does provide, but you can`t open the URL, seems that something goes wrong in Weibo.cn)*, which is severely needed in our requirements.
 </br>Maybe I should still go back to Weibo.com ...</br>
 What a bad day!!!
+
+#### 2.24
+Create a list with info. just name and twitter context *(whole twitter, solve the problem of not showing completely----"...", like this)*.</br>
+Complete simple file operation.</br>
+
+	Way to Realize:
+		Python File Operation:
+			encode&decode to write&read binary information in txt.
+			(avoid Spidering all the time while debugging)
+		BeautifulSoup Learning:
+			simple operation, really similar to XPath, which I`ve used before.
+
+I find that Weibo.cn provide LOCATION information, but the rule is complicated. Anyway, I can go back on Weibo.cn.</br>
+Cookies changes frequently, so it is not a good idea to use it in long-term project.</br>
+Twitter context havent been analysized, now it is just a 'span' div.
+
--- sina.py
+++ sina.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 
 def setCookie():
-    f = open(r'C:\Users\liuzhili\Desktop\cookie.txt', 'r')  # 打开所保存的cookies内容文件
+    f = open(r'C:\Users\liuzhili\Desktop\SpiderNeeded\cookie.txt', 'r')  # 打开所保存的cookies内容文件
     cookies = {}  # 初始化cookies字典变量
     for line in f.read().split(';'):  # 按照字符：进行划分读取
         # 其设置为1就会把字符串拆分成2份
@@ -12,7 +12,6 @@ def setCookie():
     return cookies
 
 def initParam(keyword,page):
-    # keywordCoded=
     param={'hideSearchFrame': '',
            'keyword': keyword,
            'advancedfilter': '1',
@@ -21,15 +20,57 @@ def initParam(keyword,page):
            'page': str(page)}
     return param
 
+def saveFile(data):
+    save_path = r'C:\Users\liuzhili\Desktop\SpiderNeeded\output.txt'
+    f_obj = open(save_path, 'wb')
+    f_obj.write(data)
+    f_obj.close()
+
+def readFile():
+    read_path = r'C:\Users\liuzhili\Desktop\SpiderNeeded\output.txt'
+    f_obj = open(read_path, 'rb')
+    ans = f_obj.read()
+    return ans
+
+def printList(list_all):
+    for small_list in list_all:
+        for key, value in small_list.items():
+            print("key:  " + key + "    value:   " + value + '/n')
 
 cookieUsing = setCookie()
-page = 1
-while page < 51:
-    paramsUsing = initParam('交通', page)
-    res = requests.get("http://weibo.cn/search/mblog", cookies=cookieUsing, params=paramsUsing)
+# page = 1
+# while page < 2:
+    # paramsUsing = initParam('交通', page)
+    # res = requests.get("http://weibo.cn/search/mblog", cookies=cookieUsing, params=paramsUsing)
+    # soup = BeautifulSoup(res.text)
+    # saveFile(soup.encode('UTF-8'))
 
+    # page += 1
+file = readFile().decode('UTF-8')
 
-    soup = BeautifulSoup(res.text)
+soup = BeautifulSoup(str(file), "html.parser")
 
-    print(soup.body.text)
-    page += 1
+list_all = []
+for item in soup.findAll('div', attrs={"class": "c", 'id': True}):
+    # print(item)
+    list_single = {}
+    user_name = item.find('a').string
+    blog_context = ""
+
+    whole = False
+    for inner_item in item.find('span').findAll('a'):
+        if inner_item.string == "全文":
+            whole_blog_url = requests.get("http://weibo.cn" + inner_item['href'], cookies=cookieUsing)
+            # print("Coming in")
+            inner_soup = BeautifulSoup(whole_blog_url.text, "html.parser")
+            inner_inner_item = inner_soup.find('div', attrs={'class': "c", 'id': "M_"}).find('span')
+            whole = True
+            blog_context += str(inner_inner_item)
+    if not whole:
+        blog_context += str(item.find('span'))
+
+    list_single['blog'] = blog_context
+    list_single['name'] = user_name
+
+    list_all.append(list_single)
+printList(list_all)
